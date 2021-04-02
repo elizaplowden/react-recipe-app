c71ed3852ddf39a23747397d7cf652b7ce88dfb3
Eliza Plowden <elizaplowden@gmail.com>
2021-04-02 11:53:51 +0100

    added props to Recipe

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import Recipe from './Recipe';
 
 const App = () => {
   const APP_ID = 'a38b295f';
@@ -7,31 +8,66 @@ const App = () => {
 
   // const [counter, setCounter] = useState(0);
 
+  // everything from API is stored in recipes
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  // state that only submits itself after we click the search/submit button
+  // finished text we want to add
+  const [query, setQuery] = useState('chicken');
 
   useEffect(() => {
     getRecipes();
-  }, []);
+    // will only run when we hit submit button
+  }, [query]);
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
-    const data = response.json()
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json()
     setRecipes(data.hits);
+    console.log(data.hits);
+  };
+
+  // every time you run an onChange, you'll get this event
+  const updateSearch = event => {
+    // access target from this event
+    setSearch(event.target.value);
+    console.log(search);
+  }
+
+  const getSearch = event => {
+    // stops page refresh
+    event.preventDefault();
+    // finished updated value in our input
+    setQuery(search);
+    setSearch('');
   }
 
   return (
     <div className="App">
-      <form className="search-form">
-        <input type="text" className="search-bar"/>
+      <form
+        onSubmit={getSearch}
+        className="search-form">
+        <input type="text"
+        className="search-bar"
+        value={search}
+        onChange={setSearch}
+        />
         <button
           type="submit"
           className='search-button'>
-          search
+          Search
           </button>
       </form>
-     {/* <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>*/}
+     {recipes.map(recipe => (
+        <Recipe
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          ingredients={recipe.recipe.ingredients}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}/>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
