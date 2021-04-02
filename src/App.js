b525abd3bd6903421dd3112941e5d4215a7520d1
Eliza Plowden <elizaplowden@gmail.com>
2021-04-02 11:23:46 +0100

    seeded app with recipes from adamam api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const APP_ID = 'a38b295f';
+  const APP_KEY = '6a6f21f6b409156ede94357ed003326d';
+
+  // const [counter, setCounter] = useState(0);
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = response.json()
+    setRecipes(data.hits);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input type="text" className="search-bar"/>
+        <button
+          type="submit"
+          className='search-button'>
+          search
+          </button>
+      </form>
+     {/* <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>*/}
     </div>
   );
 }
